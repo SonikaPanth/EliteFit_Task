79abe33aeb5340049b3cac14c42885e4e749ee08
SonikaPanth <sonikapanth782000@gmail.com>
2024-11-14 04:10:35 +0530

    updated

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -1,13 +1,16 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faListCheck } from '@fortawesome/free-solid-svg-icons';
+
 function Navbar() {
-    return (
-      <nav className="bg-gray-600 text-white p-4 shadow-md ">
-        <div className="max-w-6xl mx-auto flex items-center justify-between">
-          <h1 className="text-[40px] font-semibold ">Task Manager</h1>
-          
-        </div>
-      </nav>
-    );
-  }
-  
-  export default Navbar;
-  
+  return (
+    <nav className="bg-blue-600 text-white p-4 shadow-md ">
+      <div className="max-w-6xl mx-auto flex gap-4">
+        {/* Icon Component */}
+        <FontAwesomeIcon icon={faListCheck} className="text-white text-3xl mr-2 mt-6" />
+        <h1 className="text-[40px] font-semibold mt-2 ">Task Manager</h1>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
