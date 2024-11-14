fa9cc9607fb3f48c9c4468621cb5ed35799857ee
SonikaPanth <sonikapanth782000@gmail.com>
2024-11-14 09:11:14 +0530

    update

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -5,7 +5,7 @@ function Navbar() {
   return (
     <nav className="bg-blue-600 text-white p-4 shadow-md ">
       <div className="max-w-6xl mx-auto flex gap-4">
-        
+        {/* font icon */}
         <FontAwesomeIcon icon={faListCheck} className="text-white text-3xl mr-2 mt-6" />
         <h1 className="text-[40px] font-semibold mt-2 ">Task Manager</h1>
       </div>
